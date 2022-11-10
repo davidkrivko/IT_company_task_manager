55a0acb95aa79ae6f4135524a7d5d7b9a4fc16bb
Davyd Kryvko <davidkrivko@gmail.com>
2022-11-10 23:30:13 +0100

    Add models of database to manager/models.py
    TaskType models were created with a name field, Position with a name field, Worker which is inherited from AbstractUser with an additional Position field that links a table to another. The Task table was also created, which is connected to the TaskType table by a many-to-one relationship, with the Worker table - many-to-many.

--- manager/models.py
+++ manager/models.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}, {self.position}({self.username})"
+
+
+class Task(models.Model):
+    PRIORITY_LEVEL_CHOICES = (
+        "high priority",
+        "medium priority",
+        "low priority",
+    )
+
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    deadline = models.DateField()
+    is_completed = models.BooleanField()
+    priority = models.CharField(
+        max_length=15,
+        choices=PRIORITY_LEVEL_CHOICES,
+        default="medium priority"
+    )
+    task_type = models.ForeignKey(TaskType, on_delete=models.SET_NULL)
+    assignees = models.ManyToManyField(Worker)
