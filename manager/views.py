e2e8ae5fed31977630c88c4264ffb445447d634b
Davyd Kryvko <davidkrivko@gmail.com>
2022-11-17 10:49:30 +0100

    Add data to the index View and fill index.html

--- manager/views.py
+++ manager/views.py
@@ -1,12 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
+
+from manager.models import Task
 
 
 def index(request):
     num_visits = request.session.get("num_visits", 0)
     request.session["num_visits"] = num_visits + 1
 
+    num_workers = get_user_model().objects.count()
+    num_completed_task = Task.objects.filter(is_completed=True).count()
+    num_not_completed_task = Task.objects.filter(is_completed=False).count()
+
     context = {
-        "num_visits": num_visits
+        "num_visits": num_visits,
+        "num_workers": num_workers,
+        "num_completed_task": num_completed_task,
+        "num_not_completed_task": num_not_completed_task,
     }
 
     return render(request, "manager/index.html", context=context)
